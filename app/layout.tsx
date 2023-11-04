36536b8430d9c67d24ad0d0f6235f59a9065f542
ri1ken <ilvanime121000@gmail.com>
2023-11-04 19:18:44 +0900

    feat: bump openGraph information

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,24 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Welcome to Arknight Endfield',
-  description: 'Entry page of Endfield.'
+  metadataBase: new URL('https://call-me-doctor.vercel.app'),
+  openGraph: {
+    title: 'Welcome to Arknight Endfield',
+    description:
+      "Have you heard the cries of the children? Have you felt the city falling apart? Haven't you noticed the plotters smirking in triumph?",
+    url: 'https://call-me-doctor.vercel.app',
+    siteName: 'Endfield',
+    images: [
+      {
+        url: 'https://endfield.vercel.app/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Endfield OG Image'
+      }
+    ],
+    locale: 'ja_JP',
+    type: 'website'
+  }
 };
 
 export default function RootLayout({
