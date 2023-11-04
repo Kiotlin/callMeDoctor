72e2c7c3bef488e8e307516ceaede58e50f15b68
ri1ken <ilvanime121000@gmail.com>
2023-11-04 19:39:12 +0900

    fix: og image url error

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     siteName: 'Endfield',
     images: [
       {
-        url: 'https://endfield.vercel.app/og-image.png',
+        url: 'https://call-me-doctor.vercel.app/og-image.png',
         width: 1200,
         height: 630,
         alt: 'Endfield OG Image'
