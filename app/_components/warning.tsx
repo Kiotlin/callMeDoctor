f6624dfc4d5ceae4a5e71ff1ad62a5dc80c69c39
ri1ken <ilvanime121000@gmail.com>
2023-11-05 13:44:09 +0900

    feat: show warning page on mobile device

---
+++ app/_components/warning.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import React from 'react';
+
+const Warning = () => {
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-2 p-12">
+      <h1 className="text-amber-400 text-7xl text-center">WARNING</h1>
+      <Image
+        src="/FBI_WARNING.jpg"
+        width={200}
+        height={200}
+        alt=""
+        className="animate-bounce"
+      />
+      <p className="text-3xl font-bold text-center">
+        Please browse the webpage on your PC browser.
+      </p>
+    </div>
+  );
+};
+
+export default Warning;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
 import EndFieldPage from './_components/endField';
+import Warning from './_components/warning';
 import React from 'react';
+import { headers } from 'next/headers';
+
+const getMobileAgent = (): boolean => {
+  const userAgent: string | null = headers().get('user-agent');
+  const regex: RegExp =
+    /Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i;
+  return Boolean(userAgent!.match(regex));
+};
 
 export default function Home() {
-  return <EndFieldPage />;
+  const isMobile: boolean = getMobileAgent();
+  return <>{isMobile ? <Warning /> : <EndFieldPage />}</>;
 }
