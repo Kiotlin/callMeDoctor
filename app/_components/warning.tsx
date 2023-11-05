665361c28c06869bae57e786caae73e52c95fe0f
ri1ken <ilvanime121000@gmail.com>
2023-11-05 13:48:22 +0900

    style: hard code black bg and white text

--- app/_components/warning.tsx
+++ app/_components/warning.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Warning = () => {
   return (
-    <div className="h-screen w-screen flex flex-col justify-center items-center gap-2 p-12">
+    <div className="h-screen w-screen flex flex-col justify-center items-center gap-2 p-12 bg-black">
       <h1 className="text-amber-400 text-7xl text-center">WARNING</h1>
       <Image
         src="/FBI_WARNING.jpg"
@@ -12,7 +12,7 @@ const Warning = () => {
         alt=""
         className="animate-bounce"
       />
-      <p className="text-3xl font-bold text-center">
+      <p className="text-3xl font-bold text-center text-white">
         Please browse the webpage on your PC browser.
       </p>
     </div>
